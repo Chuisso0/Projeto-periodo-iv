5235931df3ba8a8f8499bbb59ea7e5d213173235
Guilherme Chuisso <gui_chuisso1@outlook.com>
2026-03-19 15:12:08 -0300

    Adicionando loader, e tab4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // 1. Quando o app abrir (caminho vazio ''), ele redireciona para o loader
+  {
+    path: '',
+    redirectTo: 'loader',
+    pathMatch: 'full',
+  },
+  // 2. A rota do seu loader que você já tinha criado
+  {
+    path: 'loader',
+    loadComponent: () => import('./pages/loader/loader.page').then(m => m.LoaderPage)
+  },
+  // 3. A rota das suas abas (tabs) continua aqui para o app usar depois que o loader terminar
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'tab4',
+    loadComponent: () => import('./tab4/tab4.page').then( m => m.Tab4Page)
   },
 ];
