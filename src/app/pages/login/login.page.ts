d56440c5437a156e98b7d468310d0a310722f6fb
Guilherme Chuisso <gui_chuisso1@outlook.com>
2026-03-19 22:22:30 -0300

    Caminho (detalhe)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonHeader, IonTitle, IonToolbar, IonItem, IonInput, IonLabel } from '@ionic/angular/standalone';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,14 @@ import { IonContent, IonButton, IonHeader, IonTitle, IonToolbar, IonItem, IonInp
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  fazerLogin() {
+
+    this.router.navigate(['/tabs/tab1']);
   }
 
 }
