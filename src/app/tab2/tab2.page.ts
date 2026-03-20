db7d2704b20268683765b3805bda6c9a1ad40149
Guilherme Chuisso <gui_chuisso1@outlook.com>
2026-03-20 16:18:46 -0300

    Avanços na area do menu, e na area de pesquisa.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonInput, IonTitle, IonContent, IonIcon, IonItem } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { searchOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonInput, IonIcon, IonTitle, IonItem, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({
+      searchOutline
+    });
+  }
 
 }
